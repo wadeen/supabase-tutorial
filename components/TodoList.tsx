36a7c3b510842488d0aaad9c0fe839f5c6ab9669
wadeen <meister.vvdo@gmail.com>
2023-03-20 01:24:31 +0900

    Modify: リストのコンポーネント分け

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,5 +1,22 @@
-const TodoList = () => {
-  return <div>Enter</div>;
+import { Database } from "@/lib/supabase";
+
+type Props = {
+  todoText: Database[];
+  onClickRemoveTodo: (id: number) => void;
+};
+
+const TodoList = ({ todoText, onClickRemoveTodo }: Props) => {
+  return (
+    <ul>
+      {todoText.map((item: Database) => (
+        <li key={item.id}>
+          {/* <input type="checkbox" checked={item.isDone} onClick={onClickTodoCheck} /> */}
+          {item.text}
+          <button onClick={() => onClickRemoveTodo(item.id!)}>削除</button>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default TodoList;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,15 @@
+import TodoList from "@/components/TodoList";
 import supabase, { Database } from "@/lib/supabase";
 import { fetchDatabase, removeSupabaseData, addSupabaseData, TABLE_NAME } from "@/lib/supabaseFunc";
 import { useEffect, useState } from "react";
-import { useRealtimeData } from "@/hooks/useRealtimeData";
+// import { useRealtimeData } from "@/hooks/useRealtimeData";
 
-export default function Home() {
+export default function Index() {
   const [inputText, setInputText] = useState(""); // 入力テキスト
   const [todoText, setTodoText] = useState<Database[]>([]); // ToDoリスト一覧
 
   // 入力テキスト
-  const onChangeInputText = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputText(() => event.target.value);
-  };
+  const onChangeInputText = (event: React.ChangeEvent<HTMLInputElement>) => setInputText(() => event.target.value);
 
   // ToDoの追加
   const onSubmitAddTodo = (event: React.FormEvent<HTMLFormElement>) => {
@@ -41,11 +40,9 @@ export default function Home() {
           },
           (payload) => {
             if (payload.eventType === "DELETE") {
-              console.log("todoText: ", todoText);
               setTodoText((todoText) => todoText.filter((todo) => todo.id !== payload.old.id));
             }
             if (payload.eventType === "INSERT") {
-              console.log("todoText: ", todoText);
               const { created_at, id, isDone, text } = payload.new;
               setTodoText((todoText) => [...todoText, { created_at, id, isDone, text }]);
             }
@@ -63,13 +60,11 @@ export default function Home() {
   };
 
   useEffect(() => {
-    const todoData = async () => {
+    (async () => {
       const todo = await fetchDatabase();
       setTodoText(todo as Database[]); // '{ [x: string]: any; }[] | null'
-    };
-    todoData();
+    })();
     fetchRealtimeData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
@@ -83,19 +78,7 @@ export default function Home() {
       </form>
 
       {/* 表示エリア */}
-      {todoText ? (
-        <ul>
-          {todoText.map((item: any) => (
-            <li key={item.id}>
-              {/* <input type="checkbox" checked={item.isDone} onClick={onClickTodoCheck} /> */}
-              {item.text}
-              <button onClick={() => onClickRemoveTodo(item.id!)}>削除</button>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>Loading...</p>
-      )}
+      {todoText ? <TodoList onClickRemoveTodo={onClickRemoveTodo} todoText={todoText} /> : <p>Loading...</p>}
     </>
   );
 }
